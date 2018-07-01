a587f277faa25773999e1ea65bfc41ba81f27473
Oxana Ilyushchenko <39965522+Oxanailyushchenko@users.noreply.github.com>
2018-07-01 16:12:44 +0300

    +Validate

---
+++ form.js
@@ -0,0 +1,44 @@
+var form_button = document.getElementById("button");
+form_button.addEventListener('click', Validate);
+
+function Validate(event){
+	event.preventDefault();
+	var form_name = document.getElementById("name");
+	var form_email = document.getElementById("email");
+
+	
+	var value = form_name.value;
+
+	if (!value){
+			form_name.style.border = "1px solid red";
+	} 
+	else {
+			form_name.style.border = "1px solid gray";
+	}
+
+	var value = form_email.value;
+	if (!value){
+			form_email.style.border = "1px solid red";
+	} 
+	else {
+			form_email.style.border = "1px solid gray";
+	}
+
+	console.log(value);
+}
+
+function Display(event){
+	var form = document.getElementById("form");
+if (!submit){
+	form.style.display = "block";}
+else
+{
+	form.style.display = "none";}
+}
+
+
+form.addEventListener ('submit', Display);
+form.submit="alert('Спасибо, Ваше сообщение отправлено!')";
+
+
+
